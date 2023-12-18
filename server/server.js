ff3a1cd13ca79131b4576d1df5288909cd9da54e
Samson <samsonshukla@gmail.com>
2023-12-18 16:22:25 +0530

    added support for multiple formats of images

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import multer from "multer";
 
 import { appConfig } from "./config/appConfig.js";
 import { aiController } from "./controllers/aiController.js";
@@ -17,8 +18,12 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const PORT = process.env.PORT;
 
+// Configure multer for handling multipart/form-data
+const storage = multer.memoryStorage(); // Store files in memory
+const upload = multer({ storage: storage });
+
 // Get Gemini API Response
-app.post("/chat-with-gemini", aiController);
+app.post("/chat-with-gemini", upload.array("image", 4), aiController);
 
 // App listening
 app.listen(PORT, () => {
